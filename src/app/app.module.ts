b4d7cab3580dde333cf97f459aec7244fad9dd22
Ryan Peden <ryan04@gmail.com>
2021-02-22 13:49:29 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SpreadSheetsModule } from "@grapecity/spread-sheets-angular";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SpreadSheetsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
